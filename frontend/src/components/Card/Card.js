616b59cb2a45dfe8a09842346253a96ac8db5edb
Dey11 <shreyandey11@gmail.com>
2023-11-27 12:10:33 +0530

    Minor UI fix.

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./Card.module.css";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Card({ name, status, pickup, destination, num, autono }) {
+  const [flag, setFlag] = useState(false);
   const handleFinishRide = async () => {
+    setFlag(true);
     await axios
       .post("http://localhost:5000/api/finishRide", {
         autono,
@@ -16,6 +19,11 @@ function Card({ name, status, pickup, destination, num, autono }) {
         console.log(error);
       });
   };
+
+  useEffect(() => {
+    setFlag(false);
+  }, []);
+
   return (
     <div className={styles.usercard}>
       <h3>{status ? name : "Unavailable"}</h3>
@@ -23,6 +31,7 @@ function Card({ name, status, pickup, destination, num, autono }) {
       <p> Pickup: {pickup}</p>
       <p> Destination: {destination}</p>
       {status ? <button onClick={handleFinishRide}>Finish Ride</button> : <></>}
+      {status && flag ? "Finishing the ride..." : ""}
     </div>
   );
 }
